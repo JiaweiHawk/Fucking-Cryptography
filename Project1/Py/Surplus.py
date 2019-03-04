ef205b26967002ec861579fb69e50c79d590f492
JiaweiHawk <38878533+JiaweiHawk@users.noreply.github.com>
2019-03-04 23:42:05 +0800

    Update Surplus.py

--- Project1/Py/Surplus.py
+++ Project1/Py/Surplus.py
@@ -4,7 +4,7 @@
    ** Date:            2019-03-03 星期日 15:22:34
    ** Description:     实现中国剩余定理
    **************************************************************************************'''
-import Euclid                               #使用euc()及gcd()
+from Euclid import euc_ext                             #使用euc()及gcd()
 
 
 def surplus_input():                        #实现中国剩余定理的输入
@@ -21,43 +21,28 @@ def surplus_input():                        #实现中国剩余定理的输入
     return dic
 
 
-class surplus:                  #实现中国剩余定理
 
-    def __init__(self, data):   #进行初始化,其中x ≡ a(i) (mod m(i) )   data为字典 data = {m(i) : a(i)}
-        ms = list( data.keys() )
-        self.length = len(ms)
-        self.mod = 1
-        for i in range( self.length):
-            self.mod = self.mod * ms[i]
-            for j in range(i + 1, self.length):
-                if( Euclid.gcd(ms[i], ms[j]) != 1):
-                    self.dic = None
-                    return                      #可以将其全部换算为素数
-        self.dic = data
-
-    def alg(self):              #进行运算
-        if( self.dic == None):
-            return None
-        ms = list( self.dic.keys() )
-        M = []
-        for m in ms:
-            mul = 1
-            for other in ms:
-                if( other != m):
-                    mul = mul * other
-            M.append(mul)
-        ans = 0
-        for i in range( self.length):
-            (a, b) = Euclid.euc(M[i], ms[i])
-            ans = (ans + a * M[i] * self.dic[ms[i]]) % self.mod
-
-        return ans
+def sur_sol(dic):
+    if( dic == None):
+        return None
+    ms = list(dic.keys())
+    Ms = []
+    tmp = 1
+    for m in ms:
+        tmp = tmp * m
+        mul = 1
+        for other in ms:
+            if (other != m):
+                mul = mul * other
+        Ms.append(mul)
+    ans = 0
+    for i in range( len(ms) ):
+        (a, b) = euc_ext(Ms[i], ms[i])
+        ans = (ans + a * Ms[i] * dic[ms[i]]) % tmp
+    return ans
 
 
 
-
-
-dic = {8:3, 3:1, 5:1}
-t1 = surplus(dic)
-print(t1.alg())
+dic = surplus_input()
+print( sur_sol(dic) )
 
